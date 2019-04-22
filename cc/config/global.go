9d4824876a08037899a197972fc8012d381b21ce
bitrvmpd <e.noyer.silva@gmail.com>
2019-04-22 11:18:16 -0500

    Revert ":fire: hack: Forcing sdclang 6.0.2"
    This reverts commit 63339081af495d644d2a58324cd03f53f35609c9.

--- cc/config/global.go
+++ cc/config/global.go
@@ -116,7 +116,7 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "sdclang-602"
+	ClangDefaultVersion      = "clang-4691093"
 	ClangDefaultShortVersion = "6.0.2"
 
 	// Directories with warnings from Android.bp files.
